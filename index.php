15fd0e5cf88c1292a5e7dedd40b1f7fa63c51bdb
Leonardo <leonardocastillo1410@gmail.com>
2023-07-31 19:26:36 -0400

    Comenzando de nuevo despues de arreglar el repo

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>odontologia</title>
+</head>
+<body>
+<nav>
+			<ul>
+				<li><a href="#">Inicio</a></li>
+				<li><a href="#">Servicios</a>
+
+				<div>
+
+					<ul>
+						<li class="titulo"><a href="">Categoria #1</a></li>
+						<li><a href="">Categoria #2</a></li>
+						<li><a href="">Categoria #3</a></li>
+						<li><a href="">Categoria #4</a></li>
+						<li><a href="">Categoria #5</a></li>
+					</ul>
+
+					<ul>
+						<li class="titulo azul"><a href="">Categoria #1</a></li>
+						<li><a href="">Categoria #2</a></li>
+						<li><a href="">Categoria #3</a></li>
+						<li><a href="">Categoria #4</a></li>
+						<li><a href="">Categoria #5</a></li>
+					</ul>
+
+					<ul>
+						<li class="titulo verde"><a href="">Categoria #1</a></li>
+						<li><a href="">Categoria #2</a></li>
+						<li><a href="">Categoria #3</a></li>
+						<li><a href="">Categoria #4</a></li>
+						<li><a href="">Categoria #5</a></li>
+					</ul>
+
+					<ul>
+						<li class="titulo rojo"><a href="">Categoria #1</a></li>
+						<li><a href="">Categoria #2</a></li>
+						<li><a href="">Categoria #3</a></li>
+						<li><a href="">Categoria #4</a></li>
+						<li><a href="">Categoria #5</a></li>
+					</ul>
+
+					<ul>
+						<li class="titulo naranja"><a href="">Categoria #1</a></li>
+						<li><a href="">Categoria #2</a></li>
+						<li><a href="">Categoria #3</a></li>
+						<li><a href="">Categoria #4</a></li>
+						<li><a href="">Categoria #5</a></li>
+					</ul>
+				</div>
+
+				</li>
+				<li><a href="#">Productos</a></li>
+				<li><a href="#">Acerca de</a></li>
+				<li><a href="#">Contacto</a></li>
+			</ul>
+		</nav>
+</body>
+</html>
